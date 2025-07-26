c970c60849a53b741ec3bd17da0ebc192464c376
Ulises Cadena <ucadena07@gmail.com>
2025-07-26 08:46:26 -0500

    textfield configuration

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,29 +1,41 @@
-import { Button, TextInput, View, StyleSheet} from "react-native";
+import { Button, TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen(){
+function StartGameScreen() {
     return <View style={styles.inputeContainer}>
-        <TextInput></TextInput>
-         <PrimaryButton>Reset</PrimaryButton>
-         <PrimaryButton>Confirm</PrimaryButton>
+        <TextInput style={styles.numberInput}
+            maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
     </View>
 }
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-        inputeContainer: {
+    inputeContainer: {
         marginHorizontal: 24,
         borderRadius: 8,
         padding: 16,
         marginTop: 100,
         elevation: 1, //android
-        backgroundColor: '#72063c', 
+        backgroundColor: '#72063c',
         shadowColor: 'black', // IOS 4 props required to create shadow
-        shadowOffset:{
-            width: 1,height:1
+        shadowOffset: {
+            width: 1, height: 1
         },
         shadowRadius: 6,
         shadowOpacity: 0.4
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
     }
 })
