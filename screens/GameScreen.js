bdf6e9ed0cf84f0f81710eb37c872e4c9dd5e1e2
Ulises Cadena <ucadena07@gmail.com>
2025-07-30 07:00:52 -0500

    starting to work on the Game Screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,23 @@
-import {text} from 'react-native'
+import {Text,StyleSheet,View} from 'react-native'
 
 function GameScreen(){
+    return (
+        <View>
+            <Text>Oppenent's Guess</Text>
+            <View>
+                <Text>Higher or lower?</Text>
+            </View>
+            <View>Log Rounds</View>
+        </View>
+    )
 
 }
 
 export default GameScreen;
+
+const styles =  StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 12
+    }
+})
